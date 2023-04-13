d1de529c393c1ee9412cbbdc945efbc2f91c3f72
LiYaokun97 <nukoayil@qq.com>
2023-04-13 15:24:43 +0200

    [feat] add GET /winedata and GET /winedata/:id

---
+++ server/winedata/winedata.model.js
@@ -0,0 +1,32 @@
+import * as fs from "fs/promises";
+const WINEDATA_FILE = "./winedata/winedata.json";
+
+// return all customer from file
+export async function getAll() {
+  try {
+    let winesTxt = await fs.readFile(WINEDATA_FILE);
+    let wines = JSON.parse(winesTxt);
+    return wines;
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      // file does not exits
+      await save([]); // create a new file with ampty array
+      return []; // return empty array
+    } // // cannot handle this exception, so rethrow
+    else throw err;
+  }
+}
+
+function findWine(wineArray, Id) {
+  return wineArray.findIndex(
+    (currWine) => currWine.wineid === Id
+  );
+}
+
+// get gustomer by ID
+export async function getByID(wineId) {
+  let wineArray = await getAll();
+  if (wineId < 0 || wineId >= wineArray.length)
+    throw new Error(`Wine with ID:${wineId} doesn't exist`);
+  else return wineArray[wineId];
+}
