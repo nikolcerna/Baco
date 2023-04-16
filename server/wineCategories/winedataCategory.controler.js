b77d800c01f00ca1d842c9618ca918c68ad4e8b9
SimonaTesinska <sim.tesinska@gmail.com>
2023-04-16 22:14:47 +0200

    other categories (origin, year) made

--- server/wineCategories/winedataCategory.controler.js
+++ server/wineCategories/winedataCategory.controler.js
@@ -10,3 +10,25 @@ export async function getWineByCategory (req, res) {
     res.status(400).send(error.message);
   }
 }
+
+export async function getWineByCountry (req, res) {
+  try {
+    let country = req.params.country
+    let wineByOrigin = await wineModel.getWineByOrigin(country);
+    res.json(wineByOrigin).send();
+  } catch (error) {
+    // res.statusMessage=
+    res.status(400).send(error.message);
+  }
+}
+
+export async function getWineByVintage (req, res) {
+  try {
+    let vintage = req.params.vintage
+    let WineByYear = await wineModel.getWineByYear(vintage);
+    res.json(WineByYear).send();
+  } catch (error) {
+    // res.statusMessage=
+    res.status(400).send(error.message);
+  }
+}
